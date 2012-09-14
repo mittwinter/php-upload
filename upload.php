3fa54de862a0ba08286044c6d1a3a53476787e38
Martin Wegner <dev@mittwinter.de>
2012-09-14 16:53:44 +0200

    Set page title to project's name.

--- upload.php
+++ upload.php
@@ -37,7 +37,7 @@ $maxNumFiles = PHPIniReader::get( 'max_file_uploads' );
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>Upload</title>
+		<title>php-upload</title>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 		<script type="text/javascript">
 		function appendFileUploadField() {
