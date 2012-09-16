44fad9f3bbd3092272886fd4ad09d92265b5e10a
Martin Wegner <dev@mittwinter.de>
2012-09-16 21:22:51 +0200

    Localization of functions.inc.php

--- functions.inc.php
+++ functions.inc.php
@@ -4,6 +4,9 @@
  *** retain this notice you can do whatever you want with this stuff. If we meet some
  *** day, and you think this stuff is worth it, you can buy me a beer in return.
  ***/
+
+require_once( './locale.inc.php' );
+
 function sizeToReadable( $size, $base = 1024 ) {
 	$si = array( 'B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB' );
 	if( $size == 0 ) {
@@ -22,18 +25,18 @@ function secondsToReadable( $seconds ) {
 	if( $minutes > 1 ) {
 		if( $hours > 1 ) {
 			if( $days > 1 ) {
-				return round( $days ) . ' days';
+				return round( $days ) . ' ' . _('days');
 			}
 			else {
-				return round( $hours ) . ' hours';
+				return round( $hours ) . ' ' . _('hours');
 			}
 		}
 		else {
-			return round( $minutes ) . ' minutes';
+			return round( $minutes ) . ' ' . _('minutes');
 		}
 	}
 	else {
-		return $seconds . ' seconds';
+		return $seconds . ' ' . _('seconds');
 	}
 }
 ?>
